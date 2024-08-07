f8398cd4d3e98258a36e7dc96554b7aeeef9bef9
Keegan <keeganjoubert@protonmail.com>
2024-08-07 16:29:03 +0200

    MySQL

---
+++ 0x00-MySQL_Advanced/names.sql
@@ -0,0 +1,5 @@
+CREATE TABLE names (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    score INT
+);
---
+++ 0x00-MySQL_Advanced/students.sql
@@ -0,0 +1,6 @@
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    score INT,
+    last_meeting DATE
+);
